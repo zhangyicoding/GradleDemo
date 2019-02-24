4117146c6a5e06c85833d1bb7dea65948cee00fe
Estyle <estyle@estyle.com>
2019-02-24 20:34:42 +0800

    Initial commit

--- app/src/main/java/com/estyle/gradledemo/MainActivity.kt
+++ app/src/main/java/com/estyle/gradledemo/MainActivity.kt
@@ -2,11 +2,17 @@ package com.estyle.gradledemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.content.pm.PackageManager
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val appInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
+        val umengChannel = appInfo.metaData.getString("UMENG_CHANNEL")
+        Toast.makeText(this, umengChannel, Toast.LENGTH_LONG).show()
     }
 }
